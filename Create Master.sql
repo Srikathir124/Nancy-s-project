bc46ddeef73aac51f119b9c7f7489661f6c44db1
Srikathir124 <srikathir124@gmail.com>
2022-08-24 15:54:55 +0530

    sql scripts for base and master table

---
+++ Create Master.sql
@@ -0,0 +1,10 @@
+create table Master(
+	`Associate ID` varchar(10),
+	`Associate Name` varchar(100),
+	`Home Manager ID` varchar(10),
+	`HCM Name` varchar(100),
+	`HCM Dept` varchar(50),
+	`Area` varchar(100),
+	`Leader` varchar(100),
+	`Initiative` varchar(100)
+);

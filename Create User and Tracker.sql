4b9348de62ce329f71787d7da19736429bd8adbd
Srikathir124 <srikathir124@gmail.com>
2022-08-24 16:56:19 +0530

    sql scripts for user and tracker tabl

--- Create User and Tracker.sql
+++ Create User and Tracker.sql
@@ -1,9 +1,9 @@
-create table User(
+create table Users(
 	`UserId` varchar(50) primary key,
     `Password` varchar(5),
     `RoleId` tinyint
 );
 create table Tracker(
-	`ID` int primary key,
+	`ID` int primary key auto_increment,
     `DownloadLogs` varchar(100)
 );
